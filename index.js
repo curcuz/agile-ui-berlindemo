0186819fd01289ddd9ba4f14654fb22a84b432d9
curcuz <czkzcz.cmc@gmail.com>
2016-07-05 13:47:51 +0200

    fixed influx port

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 (function() {
     'use strict';
 
+    const chalk = require('chalk');
     const influx = require('influx');
     const client = influx({
       hosts: 'http://127.0.0.1',
-      port: 8083,
+      port: 8086,
       username: 'root',
       password: 'root'
      })
 
      client.createDatabase("TIsensortag", function(err, result) {
        if (err) {
-         console.error("TIsensortag influx db creation failed: ",err);
+         console.log(chalk.red("TIsensortag influx db creation failed: ",err));
        } else {
-         console.log("TIsensortag influx db created");
+         console.log(chalk.cyan("TIsensortag influx db created"));
        }
      } );
 
-    setInterval(function functionName() {
+    setInterval(function keepalive() {
         console.log("keepalive");
     }, 60000);
 
