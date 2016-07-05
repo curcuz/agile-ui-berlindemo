3e8e2c6541a7c57f3b6678d21ba8b90d2eaa4773
curcuz <czkzcz.cmc@gmail.com>
2016-07-05 14:18:20 +0200

    make the grafana dir check silent

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Ensure mopidy folders are there
-mkdir /data/grafana || true
-mkdir /data/grafana/data || true
-mkdir /data/grafana/logs || true
+mkdir /data/grafana > /dev/null 2>&1 || true
+mkdir /data/grafana/data > /dev/null 2>&1 || true
+mkdir /data/grafana/logs > /dev/null 2>&1 || true
 
 # Enable i2c
 modprobe i2c-dev || true
