09f51a65e61e421c74d5cb2ad096d120a1656e80
curcuz <czkzcz.cmc@gmail.com>
2016-07-05 12:13:01 +0200

    fix

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 (function() {
-  'use strict';
-  setInterval(function functionName() {
-    console.log("keepalive");
-  },60000);
+    'use strict';
+
+    const influx = require('influx');
+    const client = influx({
+      hosts: 'http://127.0.0.1',
+      port: 8083
+      username: 'dbuser',
+      password: 'f4ncyp4ass',
+      database: 'my_database'
+     })
+
+    setInterval(function functionName() {
+        console.log("keepalive");
+    }, 60000);
+
 })();
