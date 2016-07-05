daeec1c677ad5b8608ce81e17361de6b5b4b06e7
curcuz <czkzcz.cmc@gmail.com>
2016-07-05 13:17:19 +0200

    first release

--- index.js
+++ index.js
@@ -6,9 +6,16 @@
       hosts: 'http://127.0.0.1',
       port: 8083
       username: 'root',
-      password: 'root',
-      database: '_internal'
+      password: 'root'
      })
+
+     client.createDatabase("TIsensortag", function(err, result) {
+       if (err) {
+         console.error("TIsensortag influx db creation failed: ",err);
+       } else {
+         console.log("TIsensortag influx db created");
+       }
+     } );
 
     setInterval(function functionName() {
         console.log("keepalive");
