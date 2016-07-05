6062a8095035b4964f279fd33f91113235ddf7a0
curcuz <czkzcz.cmc@gmail.com>
2016-07-05 13:30:37 +0200

    added bluetooth

--- start.sh
+++ start.sh
@@ -2,7 +2,13 @@
 
 # Enable i2c
 modprobe i2c-dev || true
+# Enable Bluetooth
+if ! /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -; then
+    /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow - || true
+fi
+hciconfig hci0 up || true
 
+# Start application loop
 while true; do
     node /app/index.js
 done
