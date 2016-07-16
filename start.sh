51ea49cdd5803acc069d0ada69eb575029602950
curcuz <czkzcz.cmc@gmail.com>
2016-07-16 22:32:53 +0200

    make the app work on previous rpi revisions

--- start.sh
+++ start.sh
@@ -13,10 +13,16 @@ mkdir /data/grafana/logs > /dev/null 2>&1 || true
 ls /data/grafana/logs/grafana.log > /dev/null 2>&1 || touch /data/grafana/logs/grafana.log > /dev/null 2>&1
 # Enable i2c
 modprobe i2c-dev || true
-# Enable Bluetooth
-if ! /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -; then
-    /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow - || true
+
+# Special procedure invoked only on rpi3 revision
+REV=`cat /proc/cmdline | awk -v RS=" " -F= '/boardrev/ { print $2 }'`
+if [ "$REV" = "0xa02082" ]
+  then
+    if ! /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -; then
+        /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow - || true
+    fi
 fi
+# Enable Bluetooth
 hciconfig hci0 up || true
 
 # Start application loop
