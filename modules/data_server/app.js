1ee4aaf54ca215e53d80cfb2490dd60437bb07b1
gaudi <craig@resin.io>
2016-07-18 12:21:47 +0100

    add influx methods

--- modules/data_server/app.js
+++ modules/data_server/app.js
@@ -3,85 +3,90 @@
 
     const chalk = require('chalk');
     const influx = require('influx');
-    const client = influx({
+    const express = require('express');
+    const app = express();
+    const bodyParser = require('body-parser');
+    var serverPort = process.env.DATA_SERVER_PORT || 8080;
+
+    function errorHandler(err, req, res, next) {
+      res.status(err.status || 500);
+      res.json({
+        message: err.message,
+        error: err
+      });
+    }
+
+    var client = influx({
         host: 'localhost',
         port: 8086,
         protocol: 'http',
         username: 'root',
-        password: 'root',
-        database: '_internal'
+        password: 'root'
     });
-    const express = require('express');
-    const app = express();
-    const bodyParser = require("body-parser");
-
-    let serverPort = process.env.DATA_SERVER_PORT || 3000;
-    function errorHandler(err, req, res, next) {
-      res.status(500);
-      res.render('error', { error: err });
-    }
 
     app.use(bodyParser.json());
     app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
+      res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
       next();
     });
     app.use(errorHandler);
 
     // list buckets
     app.get('/buckets', function(req, res) {
-      res.status(200).send();
+      client.getDatabaseNames(function(err, result) {
+        if(err) return next(err);
+        res.status(200).send(result);
+      });
     });
 
     // create a bucket
     app.post('/buckets', function(req, res) {
-      res.status(200).send();
+      client.createDatabase(req.body.id, function(err, result) {
+        client.createRetentionPolicy('defualt', req.body.id, '7d', 1, true, function (err, result) {
+          if (err) return next(err);
+          res.status(200).send(result);
+        })
+      });
     });
 
     // delete a bucket
     app.delete('/buckets/:bucketsId', function(req, res) {
-      res.status(200).send();
+      client.dropDatabase(req.params.bucketsId, function(err, result) {
+        if (err) return next(err);
+        res.status(200).send();
+      });
     });
 
     // purge a bucket
     app.put('/buckets/:bucketsId', function(req, res) {
+      // TODO
       res.status(200).send();
     });
 
     // get bucket details
     app.get('/buckets/:bucketsId', function(req, res) {
+      // not sure what to return here
       res.status(200).send();
     });
 
     // write data into a bucket
-    app.put('/buckets/:bucketsId/data', function(req, res) {
-      res.status(200).send();
+    app.put('/buckets/:bucketsId/:streamId/data', function(req, res) {
+      client.writePoint(req.params.streamId, req.body, null, {db : req.params.bucketsId}, function(err, response) {
+        if (err) return next(err);
+        res.status(200).send(response);
+      })
     });
 
     // read data from bucket
-    app.get('/buckets/:bucketsId/data/:limit', function(req, res) {
-      res.status(200).send();
+    app.get('/buckets/:bucketsId/:streamId/data/:limit', function(req, res) {
+      var query = 'SELECT * FROM ' + req.params.streamId + ' WHERE time > now() - ' + req.params.limit;
+      client.query([req.params.bucketsId], query, function(err, results) {
+        if (err) return next(err);
+        res.status(200).send(results);
+      })
     });
-
-/*
-    client.createDatabase("tisensortag", function(err, result) {
-        if (err) {
-            console.log(chalk.red("tisensortag influx db creation failed: ", err));
-        } else {
-            console.log(chalk.cyan("tisensortag influx db created"));
-        }
-    });
-
-    client.createDatabase("gebulb", function(err, result) {
-        if (err) {
-            console.log(chalk.red("gebulb influx db creation failed: ", err));
-        } else {
-            console.log(chalk.cyan("gebulb influx db created"));
-        }
-    });
-*/
 
     app.listen(serverPort, function() {
       console.log(chalk.cyan('Data Server listening on port '+serverPort));
