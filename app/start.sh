b9c6977678448fcaf281276a1651bba76e06fcc3
curcuz <czkzcz.cmc@gmail.com>
2016-07-19 22:36:34 +0200

    rebase from master

--- app/start.sh
+++ app/start.sh
@@ -13,11 +13,17 @@ mkdir /data/grafana/logs > /dev/null 2>&1 || true
 ls /data/grafana/logs/grafana.log > /dev/null 2>&1 || touch /data/grafana/logs/grafana.log > /dev/null 2>&1
 # Enable i2c
 modprobe i2c-dev || true
-# Enable Bluetooth
-if ! /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -; then
-    /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow - || true
+
+# Special procedure invoked only on rpi3 revision
+REV=`cat /proc/cmdline | awk -v RS=" " -F= '/boardrev/ { print $2 }'`
+if [ "$REV" = "0xa02082" ]
+  then
+    if ! /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -; then
+        /usr/bin/hciattach /dev/ttyAMA0 bcm43xx 921600 noflow - || true
+    fi
 fi
+# Enable Bluetooth
 hciconfig hci0 up || true
 
 # Start application loop
-pm2 start /app/pm2.json && pm2 save && pm2 logs
+pm2 start /usr/src/app/pm2.json && pm2 save && pm2 logs
